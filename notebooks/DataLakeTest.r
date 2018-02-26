d8842caebf3c7c925d79de5888d14030d0477eff
fforte850 <36839890+fforte850@users.noreply.github.com>
2018-02-26 12:42:46 +0100

    prova

--- notebooks/DataLakeTest.r
+++ notebooks/DataLakeTest.r
@@ -8,3 +8,11 @@ library(SparkR)
 # MAGIC spark.conf.set("dfs.adls.oauth2.client.id", "{YOUR SERVICE CLIENT ID}")
 # MAGIC spark.conf.set("dfs.adls.oauth2.credential", "{YOUR SERVICE CREDENTIALS}")
 # MAGIC spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com/c9c4d101-2487-4a99-bce1-d9079d8d573a/oauth2/token")
+
+# COMMAND ----------
+
+println("ciao")
+
+
+# COMMAND ----------
+
