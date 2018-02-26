43716b90a602c8911595798f3ec26fd74eb24cc5
fforte850 <36839890+fforte850@users.noreply.github.com>
2018-02-26 12:39:41 +0100

    Notebook per importare da data lake

---
+++ notebooks/DataLakeTest.r
@@ -0,0 +1,10 @@
+# Databricks notebook source
+library(SparkR)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC spark.conf.set("dfs.adls.oauth2.access.token.provider.type", "ClientCredential")
+# MAGIC spark.conf.set("dfs.adls.oauth2.client.id", "{YOUR SERVICE CLIENT ID}")
+# MAGIC spark.conf.set("dfs.adls.oauth2.credential", "{YOUR SERVICE CREDENTIALS}")
+# MAGIC spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com/c9c4d101-2487-4a99-bce1-d9079d8d573a/oauth2/token")
